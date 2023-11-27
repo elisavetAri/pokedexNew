e6c19c3e0d97597dde8004c652a1c9f40d02c345
elisavetAri <elisavet_ari@hotmail.co.uk>
2023-11-28 01:22:35 +0200

    Cypress

---
+++ packages/README.md
@@ -0,0 +1,15 @@
+# Pokedex Project. 
+
+## Getting Started
+it's a monorepo project build with Lerna, Nextjs, typescipt, redux, MUI
+
+### Installation
+ - npm install
+
+ ## Run the project
+ - npx lerna run dev
+  ## Automation Testing
+  - npx cypress open
+  ## Unit Test
+  - npm run test
+
--- packages/utils/type.d.ts
+++ packages/utils/type.d.ts
@@ -1,9 +1,67 @@
 type PokemonDetails = {
-    next:string,
-    count: number,
-    previous:string,
-    results:[{
+    abilities:[
+        {
+            ability:{
+                name:string,
+                url: string
+            } 
+        }
+    ],
+    base_experience: number,
+    forms:[{
         name:string,
-        url: string
-    }]
+        url: string 
+    }],
+    game_indices:[{
+        game_index:number,
+        version:{
+            name:string,
+            url: string 
+        }
+    }],
+    height: number,
+    held_items:[{
+       id:number,
+       location_area_encounters: string,
+       moves:[{
+        move:{
+            name:string,
+            url: string 
+        },
+        version_group_details:[{
+            level_learned_at: number,
+            move_learn_method:{
+                name:string,
+                url: string 
+            },
+            version_group:{
+                name:string,
+                url: string 
+            }  
+        }]
+       }] 
+    }],
+    name:string,
+    order: number,
+    sprites:{
+        back_default:string,
+        back_female:string,
+        back_shiny:string,
+        front_default:string,
+        front_female:string,
+        front_shiny:string,
+        front_shiny_female:string,
+        other:{
+            dream_world:{
+                front_default:string,
+                front_female:string,
+            },
+            home:{
+                front_default:string,
+                front_female:string,
+                front_shiny:string,
+                front_shiny_female:string,
+            }
+        }
+    }
 }
